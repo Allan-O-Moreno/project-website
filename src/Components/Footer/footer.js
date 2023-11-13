acbf0160254e21db4154f7a04cde82491b7db6f8
Allan Moreno <allanm@nevada.unr.edu>
2023-11-13 12:36:19 -0800

    Added Nav Bar

---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,47 @@
+// Footer.js
+
+import React from 'react';
+import './footer.css'; // Adjust the path based on your project structure
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-content">
+          <div className="footer-logo">
+            <img src="logo.png" alt="Logo" />
+            <span>Your Company</span>
+          </div>
+          <div className="footer-links">
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#services">Services</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+          <div className="footer-social">
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-facebook"></i>
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
